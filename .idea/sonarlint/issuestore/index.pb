3cba08e9afdab19321a4e8e2f0cfba0af28a398e
Aleksandr Timoshenko <aleksandrstim@gmail.com>
2018-06-15 11:37:18 +0300

    tcp_client_server

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+Y
+)src/main/java/tcp_client/MyTCPClient.java,9/8/98e760bbcfd1cde3df1fb57b4e7347bd2df3d501
+X
+(src/main/java/tcp_server/EchoThread.java,d/b/dbe26cb8c7f7deb261d036a5e90af671bb2733c0
+Y
+)src/main/java/tcp_server/MyTCPServer.java,0/c/0cd61df06b9b09332c3ab81fce01042f1f47f048
+W
+'src/main/java/tcp_client/TCPClient.java,b/3/b3100646cc1b2b999f7e3b0a8ba374be6ea1791b
+_
+/src/main/java/tcp_server/ThreadedTCPServer.java,4/7/47f7a13443434e1fc12f95c563ce72e42eabb87e
--- src/main/java/tcp_client/TCPClient.java
+++ src/main/java/tcp_client/TCPClient.java
@@ -2,38 +2,47 @@ package tcp_client;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import org.apache.log4j.Logger;
+import tcp_server.TCPServer;
 
 public class TCPClient {
 
-  static final org.apache.log4j.Logger CLIENT_LOGGER = org.apache.log4j.Logger
-      .getLogger(TCPClient.class);
+  public static final Logger LOGGER_CLIENT = Logger.getLogger(TCPServer.class);
+  static String IP = "127.0.0.1";
+  static final int PORT = 3248;
 
-  public static void main(String[] args) {
+
+  public static void main(String[] args) throws IOException {
 
     String modifiedSentence;
     int requestsQuantity = 0;
     final int MAX_REQUESTS = 1000;
 
-    try (Socket clientSocket = new Socket("127.0.0.1", 3248);
+    for (int i = 0; i < MAX_REQUESTS; i++) {
+      try (Socket clientSocket = new Socket(IP, PORT)) {
+
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(
-            new InputStreamReader(clientSocket.getInputStream()))) {
-      while (requestsQuantity <= MAX_REQUESTS) {
+            new InputStreamReader(clientSocket.getInputStream()));
+
         String message = String.format(" request number %d ", requestsQuantity);
-        CLIENT_LOGGER.info("Client send " + message);
+        LOGGER_CLIENT.info("Client send " + message);
+
         outToServer.writeBytes(message + "\n\r");
         outToServer.flush();
 
         modifiedSentence = inFromServer.readLine();
-        CLIENT_LOGGER.info("Response message from server " + modifiedSentence);
-
+        LOGGER_CLIENT.info("Response message from server " + modifiedSentence);
         System.out.println("FROM SERVER: " + modifiedSentence);
         requestsQuantity++;
+      } catch (IOException e) {
+        LOGGER_CLIENT.error(e);
+        throw new IOException("Connection refused");
       }
-    } catch (Exception e) {
-      CLIENT_LOGGER.error(e);
     }
   }
 }
+
